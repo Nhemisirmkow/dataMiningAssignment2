fef1e15ddcfd7264cea0a305690479bc217a55ad
Nhemisirmkow <marcin.michorzewski@gmail.com>
2020-05-20 16:33:09 +0200

    Updated the main files

--- LambdaRankModel.py
+++ LambdaRankModel.py
@@ -5,112 +5,228 @@ from datetime import timedelta, datetime
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 
+forbidden_columns = []
+
 if __name__ == "__main__":
     data_path_train = "./datasets_preprocessed/extracted_training_dataset.csv"
+
     # data_path_test = "./datasets_preprocessed/extracted_test_dataset.csv"
     data_path_test = "./datasets_preprocessed/test_set_VU_DM_added_columns.csv"
-    data_path_val = "./datasets_preprocessed/extracted_val_dataset.csv"
+    # data_path_test = "./Baptiste_datasets/2/test_data_2.csv"
 
-    data_train = pd.read_csv(data_path_train)
+    data_path_val_1 = "./datasets_preprocessed/extracted_val_1_dataset.csv"
+    data_path_val_2 = "./datasets_preprocessed/extracted_val_2_dataset.csv"
+    data_path_val_3 = "./datasets_preprocessed/extracted_val_3_dataset.csv"
+    data_path_val_4 = "./datasets_preprocessed/extracted_val_4_dataset.csv"
+    data_path_val_5 = "./datasets_preprocessed/extracted_val_5_dataset.csv"
+
+    gbm = lgb.LGBMRanker(learning_rate=0.13,
+                         n_estimators=300,
+                         num_leaves = 31,
+                         min_child_weight = 0.1,
+                         reg_alpha = 2,
+                         reg_lambda = 5,
+                         subsample = 0.855,
+                         colsample_bytree=0.9234,
+                         boosting_type="goss",
+                         min_child_samples=399,
+                         importance_type="gain",
+                         )
+
+    data_train = pd.read_csv(data_path_train, index_col=False)
+    print(data_train.shape[0])
     print("Training dataset loaded.")
 
-    y_train = data_train.pop("rank_score")
+
+    if 'rank_score' in data_train.columns:
+        y_train = data_train.pop("rank_score")
+    if 'label' in data_train.columns:
+        y_train = data_train.pop("label")
+    if ('Unnamed: 0' in data_train.columns):
+        data_train.pop('Unnamed: 0')
+    if "date_time" in data_train.columns:
+        data_train.pop("date_time")
+    if "position" in data_train.columns:
+        data_train.pop("position")
+    for name in forbidden_columns:
+        if name in data_train.columns:
+            data_train.pop(name)
     X_train = data_train
 
-    gbm = lgb.LGBMRanker()
-
-    srch_ids_train = X_train.srch_id.unique()
-
-    query_train_temp = {}
-    for i in srch_ids_train:
-        query_train_temp[i] = X_train.loc[X_train['srch_id'] == i].shape[0]
-        # query_train.append(X_train.loc[X_train['srch_id'] == i].shape[0])
-    query_train = list(query_train_temp.items())
-    query_train.sort()
-    query_train = [x for (_, x) in query_train]
+    query_train_pandas = pd.read_csv("./datasets_preprocessed/extracted_training_dataset_group_size.csv")['0']
+    query_train = list(query_train_pandas)
 
     print("Training dataset prepared.")
 
 
-    data_test = pd.read_csv(data_path_test)
-    print("Test dataset loaded.")
-
-    # y_test = data_test.pop("rank_score")
-    X_test = data_test
-
-    srch_ids_test = X_test.srch_id.unique()
-
-    # query_test = []
-    # query_test_temp = {}
-    # count = 0
-    # last_progress = -5
-    # for i in srch_ids_test:
-    #     query_test_temp[i] = X_test.loc[X_test['srch_id'] == i].shape[0]
-    #     # query_test.append(X_test.loc[X_test['srch_id'] == i].shape[0])
-    #     count += 1
-    #     progress = round(count / len(srch_ids_test) * 100, 2)
-    #     if int(progress * 100) % 500 == 0 and progress > last_progress + 4:
-    #         last_progress = progress
-    #         print(progress, "%")
-    # query_test = list(query_test_temp.items())
-    # query_test.sort()
-    # query_test = [x for (_, x) in query_test]
-
-    # query_test_pandas = pd.Series(query_test)
-    # query_test_pandas.to_csv("./datasets_preprocessed/test_set_VU_DM_added_columns_group_sizes.csv")
-    query_test_pandas = pd.read_csv("./datasets_preprocessed/test_set_VU_DM_added_columns_group_sizes.csv")['0']
-    query_test = list(query_test_pandas)
-
-    print("Test dataset prepared.")
+    data_val_1 = pd.read_csv(data_path_val_1, index_col=False)
+    print("Val 1 dataset loaded.")
 
 
-    data_val = pd.read_csv(data_path_val)
-    print("Val dataset loaded.")
+    if 'rank_score' in data_val_1.columns:
+        y_val_1 = data_val_1.pop("rank_score")
+    if 'label' in data_val_1.columns:
+        y_val_1 = data_val_1.pop("label")
+    if ('Unnamed: 0' in data_val_1.columns):
+        data_val_1.pop('Unnamed: 0')
+    if "date_time" in data_val_1.columns:
+        data_val_1.pop("date_time")
+    if "position" in data_val_1.columns:
+        data_val_1.pop("position")
+    for name in forbidden_columns:
+        if name in data_val_1.columns:
+            data_val_1.pop(name)
+    X_val_1 = data_val_1
 
-    y_val = data_val.pop("rank_score")
-    X_val = data_val
+    query_val_1_pandas = pd.read_csv("./datasets_preprocessed/extracted_val_1_dataset_group_size.csv")['0']
+    query_val_1 = list(query_val_1_pandas)
 
-    srch_ids_val = X_val.srch_id.unique()
-
-    query_val_temp = {}
-    for i in srch_ids_val:
-        query_val_temp[i] = X_val.loc[X_val['srch_id'] == i].shape[0]
-        # query_val.append(X_val.loc[X_val['srch_id'] == i].shape[0])
-    query_val = list(query_val_temp.items())
-    query_val.sort()
-    query_val = [x for (_, x) in query_val]
-
-    print("Val dataset prepared.")
+    print("Val 1 dataset prepared.")
 
 
+    data_val_2 = pd.read_csv(data_path_val_2, index_col=False)
+    print("Val 2 dataset loaded.")
+
+    if 'rank_score' in data_val_2.columns:
+        y_val_2 = data_val_2.pop("rank_score")
+    if 'label' in data_val_2.columns:
+        y_val_2 = data_val_2.pop("label")
+    if ('Unnamed: 0' in data_val_2.columns):
+        data_val_2.pop('Unnamed: 0')
+    if "date_time" in data_val_2.columns:
+        data_val_2.pop("date_time")
+    if "position" in data_val_2.columns:
+        data_val_2.pop("position")
+    for name in forbidden_columns:
+        if name in data_val_2.columns:
+            data_val_2.pop(name)
+    X_val_2 = data_val_2
+
+    query_val_2_pandas = pd.read_csv("./datasets_preprocessed/extracted_val_2_dataset_group_size.csv")['0']
+    query_val_2 = list(query_val_2_pandas)
+
+    print("Val 2 dataset prepared.")
+
+
+    data_val_3 = pd.read_csv(data_path_val_3, index_col=False)
+    print("Val 3 dataset loaded.")
+
+    if 'rank_score' in data_val_3.columns:
+        y_val_3 = data_val_3.pop("rank_score")
+    if 'label' in data_val_3.columns:
+        y_val_3 = data_val_3.pop("label")
+    if ('Unnamed: 0' in data_val_3.columns):
+        data_val_3.pop('Unnamed: 0')
+    if "date_time" in data_val_3.columns:
+        data_val_3.pop("date_time")
+    if "position" in data_val_3.columns:
+        data_val_3.pop("position")
+    for name in forbidden_columns:
+        if name in data_val_3.columns:
+            data_val_3.pop(name)
+    X_val_3 = data_val_3
+
+    query_val_3_pandas = pd.read_csv("./datasets_preprocessed/extracted_val_3_dataset_group_size.csv")['0']
+    query_val_3 = list(query_val_3_pandas)
+
+    print("Val 3 dataset prepared.")
+
+
+    data_val_4 = pd.read_csv(data_path_val_4, index_col=False)
+    print("Val 4 dataset loaded.")
+
+    if 'rank_score' in data_val_4.columns:
+        y_val_4 = data_val_4.pop("rank_score")
+    if 'label' in data_val_4.columns:
+        y_val_4 = data_val_4.pop("label")
+    if ('Unnamed: 0' in data_val_4.columns):
+        data_val_4.pop('Unnamed: 0')
+    if "date_time" in data_val_4.columns:
+        data_val_4.pop("date_time")
+    if "position" in data_val_4.columns:
+        data_val_4.pop("position")
+    for name in forbidden_columns:
+        if name in data_val_4.columns:
+            data_val_4.pop(name)
+    X_val_4 = data_val_4
+
+    query_val_4_pandas = pd.read_csv("./datasets_preprocessed/extracted_val_4_dataset_group_size.csv")['0']
+    query_val_4 = list(query_val_4_pandas)
+
+    print("Val 4 dataset prepared.")
+
+
+    data_val_5 = pd.read_csv(data_path_val_5, index_col=False)
+    print("Val 5 dataset loaded.")
+
+    if 'rank_score' in data_val_5.columns:
+        y_val_5 = data_val_5.pop("rank_score")
+    if 'label' in data_val_5.columns:
+        y_val_5 = data_val_5.pop("label")
+    if ('Unnamed: 0' in data_val_5.columns):
+        data_val_5.pop('Unnamed: 0')
+    if "date_time" in data_val_5.columns:
+        data_val_5.pop("date_time")
+    if "position" in data_val_5.columns:
+        data_val_5.pop("position")
+    for name in forbidden_columns:
+        if name in data_val_5.columns:
+            data_val_5.pop(name)
+    X_val_5 = data_val_5
+
+    query_val_5_pandas = pd.read_csv("./datasets_preprocessed/extracted_val_5_dataset_group_size.csv")['0']
+    query_val_5 = list(query_val_5_pandas)
+
+    print("Val 5 dataset prepared.")
 
     gbm.fit(X_train, y_train, group=query_train,
-        eval_set=[(X_val, y_val)], eval_group=[query_val],
+        eval_set=[(X_val_1, y_val_1),
+                  (X_val_2, y_val_2),
+                  (X_val_3, y_val_3),
+                  (X_val_4, y_val_4),
+                  (X_val_5, y_val_5)],
+        eval_group=[query_val_1,
+                    query_val_2,
+                    query_val_3,
+                    query_val_4,
+                    query_val_5],
         eval_at=[5], early_stopping_rounds=50)
-
     print("Model fit.")
 
-    test_pred = gbm.predict(X_test, group=query_test)
-
-    print("Model predicted.")
-
-    X_test["predicted_ranking"] = test_pred
-    print("Added predictions.")
-    X_predictions = X_test[['srch_id', 'prop_id', 'predicted_ranking']]
-    X_predictions = X_predictions.sort_values(by=['srch_id', "predicted_ranking"], ascending=[True,False])
-    print("Sorted predictions.")
-    X_predictions = X_predictions[['srch_id', 'prop_id']]
-    print("Extracted predictions.")
-    # result = pd.DataFrame(columns=['srch_id', 'prop_id'])
-    # count = 0
-    # for i in srch_ids_test:
-    #     X_predictions = X_test.loc[X_test['srch_id'] == i].sort_values(by=["predicted_ranking"], ascending=False)
-    #     X_predictions = X_predictions[['srch_id', 'prop_id']]
-    #     result = result.append(X_predictions)
-    #     count += 1
-    #     progress = round(count / len(srch_ids_test) * 100, 2)
-    #     if int(progress * 100) % 500 == 0:
-    #         print(round(count / len(srch_ids_test) * 100, 2), "%")
-
-    print(X_predictions)
-    X_predictions.to_csv("./result.csv", index=False)
+    # data_test = pd.read_csv(data_path_test)
+    # print("Test dataset loaded.")
+    #
+    # # y_test = data_test.pop("rank_score")
+    # # y_test = data_test.pop("label")
+    # if ('Unnamed: 0' in data_test.columns):
+    #     data_test.pop('Unnamed: 0')
+    # if "date_time" in data_test.columns:
+    #     data_test.pop("date_time")
+    # for name in forbidden_columns:
+    #     if name in data_test.columns:
+    #         left_data[name] = data_test.pop(name)
+    # X_test = data_test
+    #
+    # query_test_pandas = pd.read_csv("./datasets_preprocessed/test_set_VU_DM_group_sizes_original.csv")['0']
+    # query_test = list(query_test_pandas)
+    #
+    # print("Test dataset prepared.")
+    #
+    # test_pred = gbm.predict(X_test, group=query_test)
+    # print("Model predicted.")
+    #
+    # X_test["predicted_ranking"] = test_pred
+    # for name in forbidden_columns:
+    #     X_test['prop_id'] = left_data[name]
+    # print("Added predictions.")
+    #
+    # X_predictions = X_test[['srch_id', 'prop_id', 'predicted_ranking']]
+    # X_predictions = X_predictions.sort_values(by=['srch_id', "predicted_ranking"], ascending=[True,False])
+    # print("Sorted predictions.")
+    #
+    # X_predictions = X_predictions[['srch_id', 'prop_id']]
+    # print("Extracted predictions.")
+    #
+    #
+    # print(X_predictions)
+    # X_predictions.to_csv("./result.csv", index=False)
--- extract_data.py
+++ extract_data.py
@@ -3,44 +3,115 @@ import numpy
 import random
 from datetime import timedelta
 
-percentage = 0.20
-train_data_fraction = 0.9
-val_data_fraction = 0.2
+percentage = 1
+train_data_fraction = 0.70
+val_data_fraction = (1 - train_data_fraction) / 5
+
+def saveQueryDataset(name, data):
+    srch_ids = data.srch_id.unique()
+
+    query_temp = {}
+    count = 0
+    last_progress = -5
+    for i in srch_ids:
+        query_temp[i] = data.loc[data['srch_id'] == i].shape[0]
+        count += 1
+        progress = round(count / len(srch_ids) * 100, 2)
+        if int(progress * 100) % 500 == 0 and progress > last_progress + 4:
+            last_progress = progress
+            print(progress, "%")
+    query_train = list(query_temp.items())
+    query_train.sort()
+    query_train = [x for (_, x) in query_train]
+    query_train_pandas = pd.Series(query_train)
+    query_train_pandas.to_csv("./datasets_preprocessed/"+name+"_group_size.csv")
+    print("Finished SaveQueryDataset: " + name)
 
 if __name__ == "__main__":
+    # data_path = "./datasets_preprocessed/training_set_VU_DM_without_nan.csv"
+    # data_path = "./Baptiste_datasets/2/training_data_2.csv"
     data_path = "./datasets_preprocessed/training_set_VU_DM_added_columns.csv"
 
     # data = pd.read_csv(data_path, parse_dates=['date_time'])
     data = pd.read_csv(data_path)
+    print("Dataset loaded.")
     data.set_index(["srch_id"])
 
     srch_ids = list(set(data['srch_id']))
 
+    print("Sampling...")
     random_sample_srch_ids = list(random.sample(srch_ids, int(len(srch_ids) * percentage)))
-    extracted_train_srch_ids = list(random.sample(random_sample_srch_ids, int(len(srch_ids) * percentage * train_data_fraction)))
-    extracted_test_srch_ids = list(set(random_sample_srch_ids) - set(extracted_train_srch_ids))
 
-    extracted_val_srch_ids = list(random.sample(extracted_train_srch_ids, int(len(extracted_train_srch_ids) * val_data_fraction)))
-    extracted_train_srch_ids = list(set(extracted_train_srch_ids) - set(extracted_val_srch_ids))
+    extracted_val_1_srch_ids = list(random.sample(random_sample_srch_ids, int(len(random_sample_srch_ids) * val_data_fraction)))
+    extracted_train_srch_ids = list(set(random_sample_srch_ids) - set(extracted_val_1_srch_ids))
+
+    extracted_val_2_srch_ids = list(random.sample(extracted_train_srch_ids, int(len(random_sample_srch_ids) * val_data_fraction)))
+    extracted_train_srch_ids = list(set(extracted_train_srch_ids) - set(extracted_val_2_srch_ids))
+
+    extracted_val_3_srch_ids = list(random.sample(extracted_train_srch_ids, int(len(random_sample_srch_ids) * val_data_fraction)))
+    extracted_train_srch_ids = list(set(extracted_train_srch_ids) - set(extracted_val_3_srch_ids))
+
+    extracted_val_4_srch_ids = list(random.sample(extracted_train_srch_ids, int(len(random_sample_srch_ids) * val_data_fraction)))
+    extracted_train_srch_ids = list(set(extracted_train_srch_ids) - set(extracted_val_4_srch_ids))
+
+    extracted_val_5_srch_ids = list(random.sample(extracted_train_srch_ids, int(len(random_sample_srch_ids) * val_data_fraction)))
+    extracted_train_srch_ids = list(set(extracted_train_srch_ids) - set(extracted_val_5_srch_ids))
     print("sampled")
 
-    extracted_test_srch_ids.sort()
+    print("sorting:")
+    # extracted_test_srch_ids.sort()
+    # print("test set finished")
     extracted_train_srch_ids.sort()
-    extracted_val_srch_ids.sort()
+    print("train set finished")
+    extracted_val_1_srch_ids.sort()
+    print("Val 1 set finished")
+    extracted_val_2_srch_ids.sort()
+    print("Val 2 set finished")
+    extracted_val_3_srch_ids.sort()
+    print("Val 3 set finished")
+    extracted_val_4_srch_ids.sort()
+    print("Val 4 set finished")
+    extracted_val_5_srch_ids.sort()
+    print("Val 5 set finished")
+    print("sorted")
 
     extracted_train_dataset = pd.DataFrame()
-    extracted_test_dataset = pd.DataFrame()
-    extracted_val_dataset = pd.DataFrame()
+    extracted_val_1_dataset = pd.DataFrame()
+    extracted_val_2_dataset = pd.DataFrame()
+    extracted_val_3_dataset = pd.DataFrame()
+    extracted_val_4_dataset = pd.DataFrame()
+    extracted_val_5_dataset = pd.DataFrame()
 
-    extracted_test_dataset =data.loc[data['srch_id'].isin(extracted_test_srch_ids)]
-    print("Extracted test data set.")
     extracted_train_dataset =data.loc[data['srch_id'].isin(extracted_train_srch_ids)]
     print("Extracted train data set.")
-    extracted_val_dataset =data.loc[data['srch_id'].isin(extracted_val_srch_ids)]
+    extracted_val_1_dataset =data.loc[data['srch_id'].isin(extracted_val_1_srch_ids)]
     print("Extracted val data set.")
+    extracted_val_2_dataset =data.loc[data['srch_id'].isin(extracted_val_2_srch_ids)]
+    print("Extracted val data set.")
+    extracted_val_3_dataset =data.loc[data['srch_id'].isin(extracted_val_3_srch_ids)]
+    print("Extracted val data set.")
+    extracted_val_4_dataset =data.loc[data['srch_id'].isin(extracted_val_4_srch_ids)]
+    print("Extracted val data set.")
+    extracted_val_5_dataset =data.loc[data['srch_id'].isin(extracted_val_5_srch_ids)]
+    print("Extracted val data set.")
+
+
     extracted_train_dataset.to_csv("./datasets_preprocessed/extracted_training_dataset.csv", index=False)
     print("Saved train data set.")
-    extracted_test_dataset.to_csv("./datasets_preprocessed/extracted_test_dataset.csv", index=False)
-    print("Saved test data set.")
-    extracted_val_dataset.to_csv("./datasets_preprocessed/extracted_val_dataset.csv", index=False)
-    print("Saved val data set.")
+    extracted_val_1_dataset.to_csv("./datasets_preprocessed/extracted_val_1_dataset.csv", index=False)
+    print("Saved val 1 data set.")
+    extracted_val_2_dataset.to_csv("./datasets_preprocessed/extracted_val_2_dataset.csv", index=False)
+    print("Saved val 2 data set.")
+    extracted_val_3_dataset.to_csv("./datasets_preprocessed/extracted_val_3_dataset.csv", index=False)
+    print("Saved val 3 data set.")
+    extracted_val_4_dataset.to_csv("./datasets_preprocessed/extracted_val_4_dataset.csv", index=False)
+    print("Saved val 4 data set.")
+    extracted_val_5_dataset.to_csv("./datasets_preprocessed/extracted_val_5_dataset.csv", index=False)
+    print("Saved val 5 data set.")
+
+    saveQueryDataset("extracted_training_dataset", extracted_train_dataset)
+    saveQueryDataset("extracted_val_1_dataset", extracted_val_1_dataset)
+    saveQueryDataset("extracted_val_2_dataset", extracted_val_2_dataset)
+    saveQueryDataset("extracted_val_3_dataset", extracted_val_3_dataset)
+    saveQueryDataset("extracted_val_4_dataset", extracted_val_4_dataset)
+    saveQueryDataset("extracted_val_5_dataset", extracted_val_5_dataset)
